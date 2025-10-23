6501f77eed47bc1fca4a75ddabcf745953387d0f
Yizbin <148611835+Yizbin@users.noreply.github.com>
2025-10-22 23:17:54 -0600

    Creacion pantallas
    Se crearon las pantallas

---
+++ negocios_gestionOrdenes/src/main/java/gestionOrdenes/IManejoOrdenes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package gestionOrdenes;
+
+import dto.OrdenDTO;
+
+/**
+ *
+ * @author Abraham Coronel
+ */
+public interface IManejoOrdenes {
+
+    public void crearOrden(OrdenDTO orden);
+}
---
+++ negocios_gestionOrdenes/src/main/java/gestionOrdenes/ManejoOrdenes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package gestionOrdenes;
+
+import dto.OrdenDTO;
+
+/**
+ *
+ * @author Abraham Coronel
+ */
+public class ManejoOrdenes implements IManejoOrdenes{
+
+    @Override
+    public void crearOrden(OrdenDTO orden) {
+    }
+    
+    
+}
