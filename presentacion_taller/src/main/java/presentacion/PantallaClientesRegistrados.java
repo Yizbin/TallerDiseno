a81fe722b6ecd40af4dcc47b3ceb09e9efbeaf29
Andrea <andrea.melendrez252774@potros.itson.edu.mx>
2025-10-29 00:52:51 -0700

    quite 3 lineas jeje

--- presentacion_taller/src/main/java/presentacion/PantallaClientesRegistrados.java
+++ presentacion_taller/src/main/java/presentacion/PantallaClientesRegistrados.java
@@ -136,11 +136,7 @@ public class PantallaClientesRegistrados extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PantallaClientesRegistrados().setVisible(true);
-            }
-        });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
